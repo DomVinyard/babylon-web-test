92ad36ab31cee44b295e9a9e5d3cdc25f9cd926e
Dom <domvinyard@me.com>
2019-11-12 17:54:49 +0000

    init

--- src/App.js
+++ src/App.js
@@ -1,50 +1,174 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import React, { useState } from "react";
+import logo from "./logo.png";
+import styled from "styled-components";
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
+const App = () => {
+  const [selectedConsultant, setSelectedConsultant] = useState("nurse");
+  const [timeslot, setTimeslot] = useState(false);
+  const [symptoms, setSymptoms] = useState("");
+  const consultants = ["doctor", "nurse", "specialist", "therapist"];
+  const timeslots = ["Today 12:00", "12:15", "12:30"];
+  const clearAll = () => {
+    setSelectedConsultant(false);
+    setTimeslot(false);
+  };
+  return (
+    <div>
+      <Header>
+        <img src={logo} alt="logo" onClick={() => clearAll()} />
+      </Header>
+      <Main hasselected={!!selectedConsultant}>
+        {consultants.map(consultant => {
+          const isSelected = selectedConsultant === consultant;
+          // if (timeslot && !isSelected) return null;
+          return (
+            <Avatar
+              key={consultant}
+              src={`/${consultant}.png`}
+              onClick={() => setSelectedConsultant(consultant)}
+              alt={consultant}
+              hastimeslot={(!!timeslot).toString()}
+              isselected={isSelected.toString()}
+            />
+          );
+        })}
+        {!selectedConsultant ? (
+          <SelectText>select a consultant</SelectText>
+        ) : !timeslot ? (
+          <Buttons>
+            <Clear onClick={() => clearAll()}>X</Clear>
+            {timeslots.map(slot => (
+              <Timeslot onClick={() => setTimeslot(slot)}>{slot}</Timeslot>
+            ))}
+          </Buttons>
+        ) : (
+          <Buttons>
+            <Symptoms
+              onKeyUp={e => setSymptoms(e.target.value)}
+              autoFocus
+              placeholder="Describe your symptoms"
+            ></Symptoms>
+            <div>
+              <Clear onClick={() => clearAll()}>X</Clear>
+              <Timeslot
+                onClick={() => {
+                  alert(
+                    JSON.stringify({ selectedConsultant, timeslot, symptoms })
+                  );
+                  clearAll();
+                }}
+              >
+                Confirm {selectedConsultant} at {timeslot}
+              </Timeslot>
+            </div>
+          </Buttons>
+        )}
+      </Main>
+    </div>
+  );
+};
+
+const Symptoms = styled.textarea`
+  font-size: 1.6rem;
+  padding: 1rem;
+  width: 600px;
+  margin-bottom: 2rem;
+  height: 300px;
+`;
+
+const Buttons = styled.div`
+  margin-top: 5rem;
+`;
+
+const Button = styled.button`
+  font-size: 1.7rem;
+  padding: 1rem 2rem;
+  border-radius: 2rem;
+  background: #fff;
+  border: 1px solid #acacac;
+  color: #777;
+  margin: 0 0.5rem;
+  cursor: pointer;
+  &:hover {
+    background: rgb(0, 200, 200);
+    color: white;
   }
+`;
 
-  handleClick = api => e => {
-    e.preventDefault()
+const Timeslot = styled(Button)``;
 
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
+const Clear = styled(Button)`
+  color: #ccc;
+  &:hover {
+    background: black;
+    color: white;
   }
+`;
 
-  render() {
-    const { loading, msg } = this.state
+const SelectText = styled.h1`
+  color: rgb(0, 200, 200);
+  font-family: sans-serif;
+  margin-top: 3rem;
+  font-size: 2.5rem;
+`;
 
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
+const Header = styled.header`
+  img {
+    width: 144px;
+    cursor: pointer;
   }
-}
+  border-bottom: 1px solid #ccc;
+  padding: 0.5rem 2rem 0.75rem 2rem;
+  z-index: 1;
+  position: relative;
+`;
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
-      </div>
-    )
+const Main = styled.main`
+  text-align: center;
+  transition: all 0.3s;
+
+  ${({ hasselected }) =>
+    hasselected &&
+    `
+    margin-top: -2rem;
+    transform: scale(0.7);
+    img {
+      transform: scale(0.8);
+      margin: 0 0.5rem;
+    }
+  `}
+`;
+
+const Avatar = styled.img`
+  height: 300px;
+  cursor: pointer;
+  transition: all 0.3s;
+  ${({ hastimeslot, isselected }) =>
+    hastimeslot === "true" &&
+    isselected == "false" &&
+    `
+    transform: scale(0) !important;
+    opacity: 0;
+    height: 0;
+    `};
+  ${({ isselected, hastimeslot }) =>
+    isselected === "false"
+      ? `
+    -webkit-filter: grayscale(100%);
+    filter: greyscale(100%);
+    `
+      : hastimeslot === "true"
+      ? `
+    transform: scale(1.4) !important;
+      margin-top: -4rem !important;
+    `
+      : `
+      transform: scale(1.1) !important;
+    `}
+  &:hover {
+    filter: none;
+    -webkit-filter: none;
   }
-}
+`;
 
-export default App
+export default App;
